5fa8ad594bc12d3706e51a17b1f01505d42ee23c
Sagar Thapaliya <sagar.thapaliya@intel.com>
2021-03-16 16:16:25 -0500

    ch4: add hints for sender and reveiver vci selection
    These comm info hints allow user to directly select specific
    vci for sends and receives on a given communicator. These hints
    will be utilized under the implicit vci selection mode (i.e.,
    vci_method=implicit)
    
    Applications can utilize this feature to control how messages
    are spread across the VCIs. For instance, programmers can write
    the applications so that pairs of threads communicate by using
    different communicators. Then each communicator can be mapped
    to a different sender vci and a different receiver vci by using
    these hints.
    
    Valid values for this info hints are 0 to (total vcis - 1).

--- src/include/mpir_comm.h
+++ src/include/mpir_comm.h
@@ -91,6 +91,8 @@ enum MPIR_COMM_HINT_PREDEFINED_t {
     MPIR_COMM_HINT_ENABLE_MULTI_NIC_STRIPING,   /* ch4:ofi */
     MPIR_COMM_HINT_ENABLE_MULTI_NIC_HASHING,    /* ch4:ofi */
     MPIR_COMM_HINT_MULTI_NIC_PREF_NIC,  /* ch4:ofi */
+    MPIR_COMM_HINT_VCI_IDX_SENDER,      /* ch4 */
+    MPIR_COMM_HINT_VCI_IDX_RECEIVER,    /* ch4 */
     /* dynamic hints starts here */
     MPIR_COMM_HINT_PREDEFINED_COUNT
 };
